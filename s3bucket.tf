a5957052bf88007b8ebf7b0a5430d0ea9ae82976
Utkarsh Gupta <utkarsh.gupta@lidolearning.com>
2020-06-22 12:04:11 +0530

    s3bucket

---
+++ s3bucket.tf
@@ -0,0 +1,31 @@
+variable "bucket_name" { }
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "${var.bucket_name}"
+  acl    = "public-read"
+  region = "ap-south-1"
+  policy = <<EOF
+{
+  "Version":"2012-10-17",
+  "Statement":[{
+        "Sid":"PublicReadForGetBucketObjects",
+        "Effect":"Allow",
+          "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${var.bucket_name}/*"]
+    }
+  ]
+}
+EOF
+
+  force_destroy = true
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
